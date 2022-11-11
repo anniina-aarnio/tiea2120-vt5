f4594e3accba75266733bcb1e56dfe73049ef39e
Anniina Aarnio <aarnipex@student.jyu.fi>
2022-11-11 10:35:42 +0200

    Kokonaisuuden hahmottelua tyhjinä funktioina

--- vt5.js
+++ vt5.js
@@ -9,7 +9,62 @@ window.addEventListener("load", function(e) {
 	    .then(function(data) {
             console.log(data);
             // tänne oma koodi
-
+			luoSivu(data);
 	    });
 });
 
+
+
+// Kokonaiskuvan rakentaminen sivulle
+
+/**
+ * Koontifunktio, jossa luodaan sivun osat yksi kerrallaan kuntoon
+ * Osista muodostuu:
+ * - kartta, jossa näkyy rastit ja mahdollisesti joidenkin joukkueiden reitit
+ * - joukkuelistaus, jossa raahattavat joukkueennimet
+ * - kartalla-alue, johon voi raahata joukkueita ja rasteja,
+ *   minkä perusteella karttaan tulee joukkueiden reitit näkyviin
+ * - rastit-listaus, jossa raahattavat rastiennimet
+ */
+function luoSivu(data) {
+	luoKartta();	// TODO: tuleeko tämän alle omanaan vai sisälle rastien piirtäminen?
+	luoJoukkueet(data);
+	luoKartallaAlue(data);
+	luoRastit(data);
+}
+
+/**
+ * Luo kartan sivun yläosaan
+ * Keskittää näkymän niin, että koko ajan näkyy kaikki rastit,
+ * vaikka selaimen koko muuttuisi
+ */
+function luoKartta() {
+
+}
+
+/**
+ * Luo joukkuelistauksen datan perusteella aakkosjärjestykseen
+ * @param {Object} data, josta joukkueen tiedot haetaan
+ */
+function luoJoukkueet(data) {
+
+}
+
+/**
+ * 
+ * @param {Object} data 
+ */
+function luoKartallaAlue(data) {
+
+}
+
+/**
+ * Luo rastilistauksen datan perusteella aakkosjärjestykseen
+ * @param {Object} data, josta rastien tiedot haetaan
+ */
+function luoRastit(data) {
+
+}
+
+
+// 
