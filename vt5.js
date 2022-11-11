f02eb84373dada52f5601a103d880126a46d6c80
Anniina Aarnio <aarnipex@student.jyu.fi>
2022-11-11 11:22:39 +0200

    sateenkaarilista joukkueille onnistunut

--- vt5.js
+++ vt5.js
@@ -47,7 +47,16 @@ function luoKartta() {
  * @param {Object} data, josta joukkueen tiedot haetaan
  */
 function luoJoukkueet(data) {
+	let ul = document.getElementById("joukkuelista");
 
+	let lista = Array.from(data.joukkueet);
+
+	lista.forEach(function(current, index, list) {
+		let li = document.createElement("li");
+		li.textContent = current.nimi;
+		li.style.backgroundColor = rainbow(lista.length, index);
+		ul.appendChild(li);
+	});
 }
 
 /**
@@ -67,4 +76,45 @@ function luoRastit(data) {
 }
 
 
-// 
+// Joukkueen ja rastin luonnin yhteiset apufunktiot
+
+/**
+ * Joukkuelista ja rastilista luodaan lähes identtisesti
+ * Ainoastaan erona on se, miten kukin käyttäytyy eventissä
+ * @param {Array} lista, jossa objecteja joilla object.nimi (paitsi rasteilla ei ole...)
+ * @param {Function} eventfunktio, joka lisätään kullekin addEventListenerin funktioksi
+ */
+function luoSateenkaarilista(lista, eventfunktio) {
+
+}
+
+// Joukkueen luonnin apufunktioita
+
+
+
+
+// Rastien luonnin apufunktioita
+
+
+
+// Muita apufunktioita
+function rainbow(numOfSteps, step) {
+    // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
+    // Adam Cole, 2011-Sept-14
+    // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+    let r, g, b;
+    let h = step / numOfSteps;
+    let i = ~~(h * 6);
+    let f = h * 6 - i;
+    let q = 1 - f;
+    switch(i % 6){
+        case 0: r = 1; g = f; b = 0; break;
+        case 1: r = q; g = 1; b = 0; break;
+        case 2: r = 0; g = 1; b = f; break;
+        case 3: r = 0; g = q; b = 1; break;
+        case 4: r = f; g = 0; b = 1; break;
+        case 5: r = 1; g = 0; b = q; break;
+    }
+    let c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 255)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2);
+    return (c);
+}
